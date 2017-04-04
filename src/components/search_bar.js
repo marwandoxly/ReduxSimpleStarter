fd0a15eb6993600b1bc5bb6ad9c6b9ebe894324b
Marwan Nakhaleh <mnakhaleh@gmail.com>
2017-04-03 21:18:26 -0400

    condensing simple code for state change

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,12 +2,14 @@ import React, {Component} from "react";
 
 class SearchBar extends Component {
   render() {
-    return <input onChange={this.onInputChange} />;
+    return <input onChange={(e) => console.log(e.target.value)} />;
   }
 
+  /*
   onInputChange(e) { // or handleInputChange
     console.log(e.target.value);
   }
+  */
 }
 
 export default SearchBar;
